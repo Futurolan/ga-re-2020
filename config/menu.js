ee33b9f822a6c3e9de8f77552fdc7788b5b20d90
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-01-24 18:27:21 +0100

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -3,12 +3,13 @@ const menu = [
   { type: 'config', id: 'live' },
   { type: 'config', color: '#EF7C37', id: 'tickets' },
   // { type: 'page', title: 'Pass\'Bar', id: 2822, link: '/passbar' },
-  // { type: 'config', id: 'tournaments' },
-  { type: 'config', id: 'info' },
-  // { type: 'nolink',
-  //   title: 'Infos',
-  //   children: [
-  //     { type: 'config', id: 'info' },
+  { type: 'config', id: 'tournaments' },
+  // { type: 'config', id: 'info' },
+  { type: 'nolink',
+    title: 'Infos',
+    children: [
+      { type: 'config', id: 'info' },
+      { type: 'page', title: 'Comment s\'inscrire à la GA', id: 199268, link: '/inscriptions' }
   //     { type: 'page', title: 'Location', id: 3078, link: '/location' },
   //     { type: 'page', title: 'Plan', id: 3307, link: '/plan' },
   //     { type: 'config', id: 'schedule' },
@@ -18,7 +19,7 @@ const menu = [
   //     { type: 'page', title: 'Cosplay', id: 2673, link: '/cosplay' },
   //     { type: 'page', title: 'Les Offs', id: 2727, link: '/offs' },
   //     { type: 'page', title: 'Grand Poitiers', id: 1246, link: '/grand-poitiers' }
-  //   ] },
+    ] },
   // { type: 'page', title: 'Concours', id: 3175, link: '/concours' },
   { type: 'config', id: 'partners' }
 ]
