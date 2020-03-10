362279bcc8ccc94cd76306e06df33e671504d13c
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-03-10 08:08:25 +0100

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -2,18 +2,18 @@
 const menu = [
   { type: 'config', id: 'news' },
   { type: 'config', id: 'live' },
-  { type: 'config', color: '#EF7C37', id: 'tickets' },
-  { type: 'config', id: 'tournaments' },
+  // { type: 'config', color: '#EF7C37', id: 'tickets' },
+  // { type: 'config', id: 'tournaments' },
   // { type: 'config', id: 'info' },
-  {
-    type: 'nolink',
-    title: 'Infos',
-    children: [
-      { type: 'config', id: 'info' },
-      { type: 'page', title: 'Comment s\'inscrire à la GA', id: 199268, link: '/inscriptions' },
-      { type: 'page', title: 'Location de matériel', id: 199780, link: '/location' }
-    ]
-  },
+  // {
+    // type: 'nolink',
+    // title: 'Infos',
+    // children: [
+      // { type: 'config', id: 'info' },
+      // { type: 'page', title: 'Comment s\'inscrire à la GA', id: 199268, link: '/inscriptions' },
+      // { type: 'page', title: 'Location de matériel', id: 199780, link: '/location' }
+    // ]
+  // },
   { type: 'config', id: 'partners' }
 ]
 
